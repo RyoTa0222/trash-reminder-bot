6ea1a83381fcbda39f18b2639e7151e57820e9d9
Matsumoto Ryota <awpjdmga1943@icloud.com>
2021-04-29 13:00:15 +0900

    import からrequiredに変更

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,42 @@
-import express from "express";
-import line, {
-  MessageEvent,
-  WebhookEvent,
-  ClientConfig,
-  MiddlewareConfig,
-  TextEventMessage,
-  Config,
-} from "@line/bot-sdk";
-import serverless from "serverless-http";
+// import express from "express";
+// import line, {
+//   MessageEvent,
+//   WebhookEvent,
+//   ClientConfig,
+//   MiddlewareConfig,
+//   TextEventMessage,
+//   Config,
+// } from "@line/bot-sdk";
+// import serverless from "serverless-http";
+const express = require("express");
+const line = require("@line/bot-sdk");
+const serverless = require("serverless-http");
 import { DEFAULT_MESSAGE } from "./consts/config";
 
 const app = express();
 
-const lineConfig: Config = {
+const lineConfig = {
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.ACCESS_TOKEN,
 };
 
 const router = express.Router();
 router.get("/", (req, res) => res.send("Hello LINE BOT!(GET)"));
-router.post(
-  "/webhook",
-  line.middleware(lineConfig as MiddlewareConfig),
-  (req, res) => {
-    Promise.all(req.body.events.map(eventHandler)).then((result) =>
-      res.json(result)
-    );
-  }
-);
+router.post("/webhook", line.middleware(lineConfig), (req, res) => {
+  Promise.all(req.body.events.map(eventHandler)).then((result) =>
+    res.json(result)
+  );
+});
 
-const client = new line.Client(lineConfig as ClientConfig);
+const client = new line.Client(lineConfig);
 
-const eventHandler = (event: WebhookEvent) => {
+const eventHandler = (event) => {
   if (event.type !== "message" || event.message.type !== "text") {
-    return client.replyMessage(
-      (event as MessageEvent).replyToken,
-      DEFAULT_MESSAGE
-    );
+    return client.replyMessage(event.replyToken, DEFAULT_MESSAGE);
   }
-  return client.replyMessage((event as MessageEvent).replyToken, {
+  return client.replyMessage(event.replyToken, {
     type: "text",
-    text: ((event as MessageEvent).message as TextEventMessage).text,
+    text: event.message.text,
   });
 };
 
